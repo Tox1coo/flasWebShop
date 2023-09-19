a843d03986c412900e6a414f22240672e0babba9
Daniil <www.daniil_.ru@mail.ru>
2023-09-19 22:30:05 +0700

    initialization index file

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
-
+from flask import render_template
+import requests
 app = Flask(__name__)
 
+BASE_URL = "https://fakestoreapi.com/products"
 
+request = requests.get(BASE_URL)
+
+result = request.json()
+
+print(result)
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return render_template("pages/index.html",
+                            title = "FlaskShop"
+                           )
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
